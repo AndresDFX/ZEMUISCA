dcf3a969c2be8b6d84766cbbad5f5f7654831fdf
AndresDFX <andres_dfx@hotmail.com>
2020-12-08 02:49:01 -0500

    Fix

--- app/src/main/java/com/main/zemuisca/MainActivity.java
+++ app/src/main/java/com/main/zemuisca/MainActivity.java
@@ -1,242 +1,67 @@
 package com.main.zemuisca;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.text.TextUtils;
-import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.*;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
-import com.google.android.gms.auth.api.signin.GoogleSignIn;
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
-import com.google.android.gms.auth.api.signin.GoogleSignInClient;
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
-import com.google.android.gms.common.api.ApiException;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseAuthUserCollisionException;
-import com.google.firebase.auth.FirebaseUser;
+import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
+	private static final Class[] CLASSES = new Class[]{
+			EmailPasswordActivity.class,
+			GoogleSignInActivity.class,
+			FacebookLoginActivity.class,
+			TwitterLoginActivity.class,
+			AnonymousAuthActivity.class,
+			PhoneAuthActivity.class,
+			ManageUserActivity.class
+	};
 
-    private static final String TAG = MainActivity.class.getSimpleName(); ;
-    //defining view objects
-    private EditText TextEmail;
-    private EditText TextPassword;
-    private Button btnRegistrar;
-    private Button btnRegistrarGoogle;
-    private Button btnIniciar;
-    private ProgressDialog progressDialog;
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		ListView listView = findViewById(R.id.list_view);
 
-    //Declaramos un objeto firebaseAuth
-    private FirebaseAuth firebaseAuth;
-    private GoogleSignInOptions gso;
-    private GoogleSignInClient mGoogleSignInClient;
+		MyArrayAdapter adapter = new MyArrayAdapter(this, android.R.layout.simple_list_item_1, CLASSES);
+		listView.setAdapter(adapter);
+		listView.setOnItemClickListener(this);
+	}
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+	@Override
+	public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+		Class clicked = CLASSES[position];
+		startActivity(new Intent(this, clicked));
+	}
 
+	private static class MyArrayAdapter extends ArrayAdapter<Class> {
+		private Context mContext;
+		private Class[] mClasses;
 
-        // Configure sign-in to request the user's ID, email address, and basic
-        // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
-         gso= new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                .requestEmail()
-                .build();
+		private MyArrayAdapter(Context context, int resource, Class[] objects) {
+			super(context, resource, objects);
+			mContext = context;
+			mClasses = objects;
+		}
 
-        //inicializamos el objeto firebaseAuth
-        firebaseAuth = FirebaseAuth.getInstance();
-        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+		@Override
+		public View getView(int position, View convertView, ViewGroup parent) {
+			View view = convertView;
 
-        // Build a GoogleSignInClient with the options specified by gso.
+			if (convertView == null) {
+				LayoutInflater inflater = (LayoutInflater) mContext.getSystemService(LAYOUT_INFLATER_SERVICE);
+				view = inflater.inflate(android.R.layout.simple_list_item_1, null);
+			}
+			((TextView) view.findViewById(android.R.id.text1)).setText(mClasses[position].getSimpleName());
 
-
-        //Referenciamos los views
-        TextEmail = (EditText) findViewById(R.id.inputEmail);
-        TextPassword = (EditText) findViewById(R.id.inputPassword);
-        btnRegistrar = (Button) findViewById(R.id.btn_registrar);
-        btnIniciar = (Button) findViewById(R.id.btn_iniciar);
-        btnRegistrarGoogle = (Button) findViewById(R.id.btn_google);
-        progressDialog = new ProgressDialog(this);
-
-        //Escuchas de elementos que tienen eventos
-
-        findViewById(R.id.btn_iniciar).setOnClickListener(this);
-        findViewById(R.id.btn_registrar).setOnClickListener(this);
-        findViewById(R.id.btn_google).setOnClickListener(this);
-
-
-    }
-
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-        Toast.makeText(this, "OnStart", Toast.LENGTH_SHORT).show();
-        // La actividad está a punto de hacerse visible.
-       GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
-       updateUI(account);
-    }
-
-    //Change UI according to user data.
-    private void updateUI(GoogleSignInAccount signedIn) {
-        if (signedIn != null) {
-            // sigin is successfull
-            btnRegistrarGoogle.setVisibility(View.GONE);
-            Toast.makeText(this, "Inicio de sesion correcto", Toast.LENGTH_SHORT).show();
-            startActivity(new Intent(MainActivity.this, WelcomeActivity.class));
-
-        } else {
-            // sigin is cancelled
-            btnRegistrarGoogle.setVisibility(View.VISIBLE);
-
-        }
-    }
-
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.btn_google:
-                signInGoogle();
-                break;
-            case R.id.btn_registrar:
-                registrarUsuario();
-                break;
-            case R.id.btn_iniciar:
-                loginUsuario();
-                break;
-
-        }
-    }
-
-    //Evento del boton
-    public void signInGoogle() {
-        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
-        startActivityForResult(signInIntent, 1);
-    }
-
-    public void registrarUsuario(){
-
-        //Obtenemos el email y la contraseña desde las cajas de texto
-        String email = TextEmail.getText().toString().trim();
-        String password  = TextPassword.getText().toString().trim();
-
-        //Verificamos que las cajas de texto no esten vacías
-        if(TextUtils.isEmpty(email)){
-            Toast.makeText(this,"Se debe ingresar un email",Toast.LENGTH_LONG).show();
-            return;
-        }
-
-        if(TextUtils.isEmpty(password)){
-            Toast.makeText(this,"Falta ingresar la contraseña",Toast.LENGTH_LONG).show();
-            return;
-        }
-
-
-        progressDialog.setMessage("Realizando registro en linea...");
-        progressDialog.show();
-
-        //creating a new user
-        firebaseAuth.createUserWithEmailAndPassword(email, password)
-                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        //checking if success
-                        if(task.isSuccessful()){
-
-                            Toast.makeText(MainActivity.this,"Se ha registrado el usuario con el email: "+ TextEmail.getText(),Toast.LENGTH_LONG).show();
-                        }
-
-                        else{
-                            Toast.makeText(MainActivity.this, task.getException().getLocalizedMessage(),Toast.LENGTH_LONG).show();
-                        }
-                        progressDialog.dismiss();
-                    }
-                });
-
-    }
-
-    /**
-     *
-
-     */
-    public void loginUsuario(){
-
-        //Obtenemos el email y la contraseña desde las cajas de texto
-        final String email = TextEmail.getText().toString().trim();
-        String password  = TextPassword.getText().toString().trim();
-
-        //Verificamos que las cajas de texto no esten vacías
-        if(TextUtils.isEmpty(email)){
-            Toast.makeText(this,"Se debe ingresar un email",Toast.LENGTH_LONG).show();
-            return;
-        }
-
-        if(TextUtils.isEmpty(password)){
-            Toast.makeText(this,"Falta ingresar la contraseña",Toast.LENGTH_LONG).show();
-            return;
-        }
-
-
-        progressDialog.setMessage("Ingresando en linea...");
-        progressDialog.show();
-
-        //cpmsultar usuario
-        firebaseAuth.signInWithEmailAndPassword(email, password)
-                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        //checking if success
-                        if(task.isSuccessful()){
-                            int pos = email.indexOf("@");
-                            String user = email.substring(0, pos);
-                            Toast.makeText(MainActivity.this, "Bienvenido: " + TextEmail.getText(), Toast.LENGTH_LONG).show();
-                            Intent intencion = new Intent(getApplication(), WelcomeActivity.class);
-                            intencion.putExtra("usuario", user);
-                            startActivity(intencion);
-
-                        }else{
-                            Toast.makeText(MainActivity.this, task.getException().getLocalizedMessage(),Toast.LENGTH_LONG).show();
-                        }
-                        progressDialog.dismiss();
-                    }
-                });
-
-    }
-
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
-        if (requestCode == 1) {
-            // The Task returned from this call is always completed, no need to attach
-            // a listener.
-            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
-            handleSignInResult(task);
-        }
-    }
-
-    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
-        try {
-            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
-
-            // Signed in successfully, show authenticated UI.
-            updateUI(account);
-        } catch (ApiException e) {
-            // The ApiException status code indicates the detailed failure reason.
-            // Please refer to the GoogleSignInStatusCodes class reference for more information.
-            Log.w(TAG, "signInResult:failed code=" + e.getStatusCode());
-            updateUI(null);
-        }
-    }
-
-
+			return view;
+		}
+	}
 }
